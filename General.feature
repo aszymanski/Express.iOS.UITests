d43e4a4fc229b196c8926872cae29a2024b8e45b
Adrian Szymanski <aszymanski@inflocalusers-MacBook-Pro-2.local>
2017-01-11 12:10:13 +0100

    Initial commit

---
+++ General.feature
@@ -0,0 +1,29 @@
+﻿Feature: General Smoke Test
+
+	Scenario: Tap Add Callendar
+		Given I can see "US Test Add Calendar Event" button
+		When  I tap "US Test Add Calendar Event" button
+	#	Then I can see "US Test Add Calendar Event" button
+
+	Scenario: Tap Go Online Forms 1
+		Given I can see "Go Online Forms 1" button
+		When I tap "Go Online Forms 1" button
+		#Then I can see "Online Form" screen
+		Given I open App
+
+	Scenario: Tap Go Online Forms 2
+		Given I can see "Go Online Forms 2" button
+		When I tap "Go Online Forms 2" button
+	#	Then I can see "Online Form" screen
+
+	#Scenario: Show PDF
+	#	Given I can see "Show PDF" button
+	#	When  I tap "Show PDF" buttoon
+	#	Then Popup appears "Do you want to open in external viewer?"
+
+	#	When I tap "Cancel" buttoon
+	#	Then I can see "PDFView" screen
+
+
+	#Scenario: First Example
+	#	Given I open App
---
+++ GeneralSteps.cs
@@ -0,0 +1,63 @@
+﻿using Xamarin.UITest;
+using Xamarin.UITest.Utils;
+using Xamarin.UITest.iOS;
+using TechTalk.SpecFlow;
+
+namespace Express.iOS.UITests
+{
+	[Binding]
+	public class GeneralSteps
+	{
+		iOSApp app;
+
+		[BeforeScenario]
+		public void SetUp()
+		{
+			//app = ConfigureApp.iOS.AppBundle("../../Express.iOS.app").DeviceIdentifier("a0f8c523e4b059cdf269d7fcf64108a7a5b63535").StartApp();
+			//app = ConfigureApp.iOS.InstalledApp("com.infusion.Express.iOS").DeviceIdentifier("a0f8c523e4b059cdf269d7fcf64108a7a5b63535").StartApp();
+			app = ConfigureApp.iOS.Debug().StartApp();
+		}
+
+		[Given("I open App")]
+		public void IOpenApp()
+		{
+			app.Repl();
+		}
+
+		[Given(@"I can see ""(.*)"" button")]
+		public void GivenICanSeeButton(string buttonName)
+		{
+			app.WaitForElement(c => c.Marked(buttonName));
+
+		}
+
+		[When(@"I tap ""(.*)"" button")]
+		public void WhenITapButton(string buttonName)
+		{
+			app.Tap(c => c.Marked(buttonName));
+		}
+
+		[Then(@"I can see ""(.*)"" button")]
+		public void ThenICanSeeButton(string buttonName)
+		{
+			app.WaitForElement(c => c.Marked(buttonName));
+		}
+
+		[Then(@"Popup appears ""(.*)""")]
+		public void ThenPopupAppear(string msg)
+		{
+			app.WaitForElement(c => c.Class("TextView").Text(msg));
+
+		}
+
+		[Then(@"I can see ""(.*)"" screen")]
+		public void ThenICanSeeScreen(string screenName)
+		{
+			app.WaitForElement(c => c.Class("TextView").Text(screenName));
+			app.ScrollDown();
+			app.ScrollDown();
+
+		}
+
+	}
+}
